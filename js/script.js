3db7c6c3145b2c89e5885eacc916d4a5934e8392
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-10 20:05:13 +0200

    add colors

--- js/script.js
+++ js/script.js
@@ -9,18 +9,22 @@ for (let i = 1; i <= 100; i++) {
     if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log('è divisibile per 3 e 5 ' + i + 'FizzBuzz');
         myDiv.append('BuzzFizz');
+        myDiv.classList.add('bg-danger');
     }
     else if (i % 3 == 0) {  
         console.log('divisibile per 3 ' + i + 'buzz');
         myDiv.append('Buzz');
+        myDiv.classList.add('bg-warning');
 
     }
     else if (i % 5 == 0) {
         console.log('divisibile per 5 ' + i + 'fizz');
         myDiv.append('Fizz');
+        myDiv.classList.add('bg-info');
 
     }else {
         myDiv.append(i);
+        myDiv.classList.add('bg-primary');
     }
     wrapper.append(myDiv);
 }
